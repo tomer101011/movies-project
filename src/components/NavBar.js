bba9aa3f9d6eb718da94bfe1f914585fe5f48cf2
Tomer <tomerst3@gmail.com>
2020-09-20 21:45:53 +0300

    Continue

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -27,7 +27,7 @@ export default class NavBar extends Component {
         return (
             <nav id="navbar" className="navbar navbar-default navbar-expand-lg navbar-light">
                 <div className="navbar-header">
-                    <a className="navbar-brand" href="/#">Movie<b>Mojo</b></a>
+                    <a className="navbar-brand" href="/">Movie<b>Mojo</b></a>
                     <button type="button" data-target="#navbarCollapse" data-toggle="collapse" className="navbar-toggle">
                         <span className="navbar-toggler-icon"></span>
                         <span className="icon-bar"></span>
@@ -37,12 +37,6 @@ export default class NavBar extends Component {
                 </div>
                 {/* Collection of nav links, forms, and other content for toggling  */}
                 <div id="navbarCollapse" className="collapse navbar-collapse">
-                    <form className="navbar-form form-inline">
-                        <div className="input-group search-box">
-                            <input type="text" id="search" className="form-control" placeholder="Search here..." />
-                            <span className="input-group-addon"><i className="material-icons">&#xE8B6;</i></span>
-                        </div>
-                    </form>
                     <ul className="nav navbar-nav navbar-right">
                         <li>
                             <a data-toggle="dropdown" className="dropdown-toggle" href="/#">Login</a>
@@ -56,9 +50,6 @@ export default class NavBar extends Component {
                                         <input type="password" className="form-control" placeholder="Password" required="required" />
                                     </div>
                                     <input type="submit" className="btn btn-primary btn-block" value="Login" />
-                                    <div className="form-footer">
-                                        <a href="/#">Forgot Your password?</a>
-                                    </div>
                                 </li>
                             </ul>
                         </li>
