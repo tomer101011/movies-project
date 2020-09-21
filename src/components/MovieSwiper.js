dd129b18c58c6e30675da4b62894c4a7d9eec50b
Tomer <tomerst3@gmail.com>
2020-09-21 15:06:10 +0300

    Added server and database connection

--- src/components/MovieSwiper.js
+++ src/components/MovieSwiper.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Navigation } from 'swiper';
+import axios from "axios";
 
 // Import Swiper styles
 import 'swiper/swiper.scss';
@@ -9,14 +10,32 @@ import 'swiper/components/navigation/navigation.scss';
 import '../styles/swiper-style.css';
 
 // install Swiper components
-SwiperCore.use([Navigation]); 
-
+SwiperCore.use([Navigation]);
 
 export default class MovieSwiper extends Component {
-    render() {
-        return (
-            <div>
-                <h2 className="panel">Recently added</h2>
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: []
+        }
+    }
+
+    componentDidMount() {
+        const url = 'http://localhost:9000/movies';
+        axios.get(url)
+            .then(res => {
+                this.setState({ movies: res.data })
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        this.loadMovies();
+    }
+
+    loadMovies = () => {
+        if (this.state.movies.length !== 0) {
+            return (
                 <Swiper
                     navigation
                     loop={true}
@@ -41,38 +60,31 @@ export default class MovieSwiper extends Component {
                         }
                     }}
                 >
-                    <SwiperSlide>
-                        <a href="/">
-                            <img style={{ maxWidth: '100%' }} alt='Space Betwen Us' src="https://www.focusoncode.com/uploads/demo/movies-trailer/images/3.jpg" />
-                            <h3 className="hometitle">Space Betwen Us</h3>
-                        </a>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img style={{ maxWidth: '100%' }} alt='' src="https://www.focusoncode.com/uploads/demo/movies-trailer/images/9.jpg" />
-                        <h3 className="hometitle">Space Betwen Us</h3>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img style={{ maxWidth: '100%' }} alt='' src="https://www.focusoncode.com/uploads/demo/movies-trailer/images/4.jpg" />
-                        <h3 className="hometitle">Space Betwen Us</h3>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img style={{ maxWidth: '100%' }} alt='' src="https://www.focusoncode.com/uploads/demo/movies-trailer/images/5.jpg" />
-                        <h3 className="hometitle">Space Betwen Us</h3>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img style={{ maxWidth: '100%' }} alt='' src="https://www.focusoncode.com/uploads/demo/movies-trailer/images/7.jpg" />
-                        <h3 className="hometitle">Space Betwen Us</h3>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img style={{ maxWidth: '100%' }} alt='' src="https://www.focusoncode.com/uploads/demo/movies-trailer/images/2.jpg" />
-                        <h3 className="hometitle">Space Betwen Us</h3>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img style={{ maxWidth: '100%' }} alt='' src="https://www.focusoncode.com/uploads/demo/movies-trailer/images/8.jpg" />
-                        <h3 className="hometitle">Space Betwen Us</h3>
-                    </SwiperSlide>
+                    {
+                        this.state.movies.map((movie, i) => {
+                            return <SwiperSlide key={i}>
+                                <a href="/">
+                                    <img style={{ maxWidth: '100%' }} alt={movie.name} src={movie.picture} />
+                                    <h3 className="hometitle">{movie.name}</h3>
+                                </a>
+                            </SwiperSlide>
+                        })
+                    }
                 </Swiper>
-            </div>
+            )
+        }
+
+    }
+
+    render() {
+        return (
+            <div>
+
+                <h2 className="panel">Recently added</h2>
+                
+                    {this.loadMovies()}
+
+            </div >
         )
     }
 }
