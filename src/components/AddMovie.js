57ea86a802dc088c77b873c1a2b3c40e1c96746e
Tomer <tomerst3@gmail.com>
2020-09-26 21:41:34 +0300

    Continue

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -1,48 +1,95 @@
 import React, { Component } from 'react'
 
-// import axios from 'axios';
-// import { server_path } from '../constants/server.js';
+import axios from 'axios';
+import { server_path } from '../constants/server.js';
 
 import '../styles/movie-info-style.css';
 import '../styles/add-movie-style.css';
 
 export default class AddMovie extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchInput: '',
+            movieInfo: '',
+            trailer: '',
+            movieId: ''
+        }
+    }
+
+    setSearchInput = (e) => {
+        this.setState({ searchInput: e.target.value });
+    }
+
+    searchOMDb = () => {
+        const urlOMDb = `${server_path}/omdb`;
+        axios.post(urlOMDb, { search: this.state.searchInput })
+            .then(res => {
+                if (res.data === 'movie not found')
+                    alert('Movie not found. Maybe the name was incorrect.');
+
+                else
+                    this.setState({
+                        movieInfo: res.data.movieInfo,
+                        trailer: res.data.trailer,
+                        movieId: res.data.movieId
+                    });
+            })
+            .catch(err => { console.log(err); })
+    }
+
+    loadRatingText = () => {
+        if (this.state.movieInfo.rating === 'N/A')
+            return <li><p><span>Metascore: </span>{this.state.movieInfo.rating}</p></li>
+        else
+            return <li><p><span>Metascore: </span>{this.state.movieInfo.rating}/100</p></li>
+    }
+
+    loadAddButton = () => {
+        if (this.state.movieInfo !== '') {
+            if (this.state.movieId.length === 0)
+                return <li><button onClick={() => alert()} id="addMovie" className="favorite">Add to the website</button></li>
+            else
+                return <li><button onClick={() => alert()} id="removeMovie" className="favorite-color favorite">Remove from the website</button></li>
+        }
+    }
+
     render() {
         return (
             <div className="wrapper">
                 <main className="content">
                     <div className="single">
                         <section className="addBox">
-                            <p className="searchHeader">Add a <span id="spanOr">new movie</span> from IMDb</p>
+                            <p className="searchHeader">Add a <span id="spanWord">new movie</span> from IMDb</p>
                             <div className="wrap">
                                 <div className="search">
-                                    <input id="searchImdb" type="text" className="searchTerm" placeholder="Search a movie" />
-                                    <button type="submit" className="searchButton">
-                                        <i className="fa fa-search"></i>
+                                    <input id="searchImdb" onChange={this.setSearchInput} type="text" className="searchTerm" placeholder="Search a movie" />
+                                    <button onClick={() => this.searchOMDb()} className="searchButton">
+                                        <i className="fa fa-search" />
                                     </button>
                                 </div>
                             </div>
                         </section>
+
                         <section className="trailer">
                             <div className="trailer-frame">
-                                <iframe title={'movie title'} width="560" height="349" src="" frameBorder="0" allowFullScreen />
+                                <iframe title={this.state.movieInfo.poster} width="560" height="349" src={this.state.trailer} frameBorder="0" allowFullScreen />
                             </div>
                         </section>
 
                         <section className="movie">
-                            <img alt='' src={'poster link'} />
+                            <img alt='' src={this.state.movieInfo.poster} />
                             <ul>
-                                {/* title is here */}
-                                <li>{}</li>
-                                {/* plot is here */}
-                                <li>{}</li>
-                                {/* <li><button onClick={() => alert()} id="fav" className="favorite">Add to the website</button></li> */}
-                                <li className="margin-genre"><p><span>Genres: </span>{}</p></li>
-                                <li><p><span>Director: </span>{}</p></li>
-                                <li><p><span>Cast: </span>{}</p></li>
-                                <li><p><span>Released: </span>{}</p></li>
-                                <li><p><span>Runtime: </span>{}</p></li>
-                                <li><p><span>Metascore: </span>{`/100`}</p></li>
+                                <li>{this.state.movieInfo.title}</li>
+                                <li>{this.state.movieInfo.plot}</li>
+                                {this.loadAddButton()}
+                                <li className="margin-genre"><p><span>Genres: </span>{this.state.movieInfo.genre}</p></li>
+                                <li><p><span>Director: </span>{this.state.movieInfo.director}</p></li>
+                                <li><p><span>Cast: </span>{this.state.movieInfo.actors}</p></li>
+                                <li><p><span>Released: </span>{this.state.movieInfo.released}</p></li>
+                                <li><p><span>Runtime: </span>{this.state.movieInfo.runtime}</p></li>
+                                {this.loadRatingText()}
                             </ul>
                         </section>
                     </div>
