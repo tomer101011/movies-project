3fd1392df1df3947760d83ebc0d9025bcacbfb91
tomer101011 <51194603+tomer101011@users.noreply.github.com>
2020-09-30 06:57:20 +0000

    Deploying to gh-pages from  @ 15ead1d749ba8c52275ddfb22d5f36d577d6cb56 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movies-project/precache-manifest.8ef77aeb1a5f0deff4c2b34f2e7b20fc.js"
+  "/movies-project/precache-manifest.1c5074c9fbd8f3bc696337326471d0d3.js"
 );
 
 self.addEventListener('message', (event) => {
