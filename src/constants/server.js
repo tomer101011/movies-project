f4940d2c8191c4b4ae87a412a9afecb95aa7ceb3
Tomer <tomerst3@gmail.com>
2020-10-01 22:24:15 +0300

    Continue

--- src/constants/server.js
+++ src/constants/server.js
@@ -1,2 +1,2 @@
-export const server_path = 'https://movie-mojo-server.herokuapp.com/';
+export const server_path = 'https://movie-mojo-server.herokuapp.com';
 // export const server_path = 'http://localhost:9000';
