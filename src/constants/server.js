69ca37e1746f18cc73308159c16cbfc25b5fd66f
Tomer <tomerst3@gmail.com>
2020-10-11 11:49:59 +0300

    Continue

--- src/constants/server.js
+++ src/constants/server.js
@@ -1,2 +1,2 @@
-//server_path is changed based on the .env file. On Github it is different then locally
+//server_path is changed based on the .env file. On Github it is different than locally
 export const server_path = process.env.REACT_APP_SERVER;
